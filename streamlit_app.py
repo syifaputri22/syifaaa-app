5578f32ad091b616f508298e07e648baf6e52a68
syifaputri22 <syifawanda52@gmail.com>
2025-05-23 08:46:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("ca07292cec71375b99aed8d460151f6d.jpg", width=200)
